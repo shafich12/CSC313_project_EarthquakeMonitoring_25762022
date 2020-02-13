c1d94e4fc7dab9cd3b34445430a49ed74285ad62
yesukay2 <yesukay2@gmail.com>
2020-02-13 23:53:41 +0000

    Made changes to Observatory class

--- projectG/Observatory.java
+++ projectG/Observatory.java
@@ -5,6 +5,7 @@ import java.util.List;
 // import Galamsey.Location;
 // import Galamsey.colour;
 public class Observatory {
+    private String observatoryName;
     private String countryName;
     private int areaCovered;
     private List<Galamsey> galamseyEvents;
@@ -14,6 +15,8 @@ public class Observatory {
         galamseyEvents = new ArrayList<>();
     }
 
+    
+
     public void setCountryName(final String countryName) {
         this.countryName = countryName;
     }
@@ -22,16 +25,6 @@ public class Observatory {
         this.areaCovered = areaCovered;
     }
 
-    public void createEvent(Galamsey.colour vegetationColour, int colourValue, Position position, int year){
-        Galamsey newEvent = new Galamsey(vegetationColour, colourValue, position, year);
-        galamseyEvents.add(newEvent);
-    }
-
-    public void viewEvents(){
-        for (Galamsey galamsey : galamseyEvents) {
-            System.out.println(galamsey.getColourValue());
-        }
-    }
 
     // public class GalamseyEvents {
     //     public galamsey(colour vegetationColour, int colourValue, Location location) {
@@ -44,6 +37,51 @@ public class Observatory {
     //         }
     //     }
     // }
+
+
+
+    public String getObservatoryName() {
+        return observatoryName;
+    }
+
+    public void setObservatoryName(String observatoryName) {
+        this.observatoryName = observatoryName;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public int getAreaCovered() {
+        return areaCovered;
+    }
+
+    public List<Galamsey> getGalamseyEvents() {
+        return galamseyEvents;
+    }
+
+    public void setGalamseyEvents(List<Galamsey> galamseyEvents) {
+        this.galamseyEvents = galamseyEvents;
+    }
+
+    public int getYearStarted() {
+        return yearStarted;
+    }
+
+    public void setYearStarted(int yearStarted) {
+        this.yearStarted = yearStarted;
+    }
+
+    public void createEvent(Galamsey.colour vegetationColour, int colourValue, Position position, int year){
+        Galamsey newEvent = new Galamsey(vegetationColour, colourValue, position, year);
+        galamseyEvents.add(newEvent);
+    }
+
+    public void viewEvents(){
+        for (Galamsey galamsey : galamseyEvents) {
+            System.out.println(galamsey.getColourValue());
+        }
+    }
 
     public static void main(String[] args) {
         Position accra = new Position(2.5, 3.6);
