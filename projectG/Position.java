1205c42e52a54123fc0186d0d49d0633a6962bf1
jadorks <adorkorj@gmail.com>
2020-02-14 22:58:34 +0000

    Moved over to IntelliJ and started javadoc

--- projectG/Position.java
+++ projectG/Position.java
@@ -2,12 +2,21 @@ public class Position{
     private double Longitude;
     private double Latitude;
 
+    /**
+     * Constructor for Position class
+     * @param Longitude : double Contains longitude value of position
+     * @param Latitude : double Contains latitude value of position
+     */
     public Position(double Longitude,double Latitude){
         this.Longitude = Longitude;
         this.Latitude = Latitude;
 
     }
 
+    /**
+     *
+     * @param Longitude double
+     */
     public void setLongitude(double Longitude){
         this.Longitude = Longitude;
     }
